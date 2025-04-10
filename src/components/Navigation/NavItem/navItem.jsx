bbbf9aa1647c038b7f3af536be4f5c301b5b2011
Endorze <alexyurihallgren@hotmail.com>
2025-04-10 16:08:35 +0200

    feat: Added phoneview menu, adjusted some css

--- src/components/Navigation/NavItem/navItem.jsx
+++ src/components/Navigation/NavItem/navItem.jsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom"
 import styles from "./navItem.module.css"
 
-const NavItem = ({path}) => {
+const NavItem = ({path, onClick}) => {
 
     const link = path.toLowerCase() === "home" ? "/" : `/${path.toLowerCase()}`;
 
     return (
         <NavLink to={link} className={({ isActive }) => isActive ? styles.active : ""
-          }>{path}</NavLink>
+          } onClick={onClick}>{path}</NavLink>
     )
 }
 
